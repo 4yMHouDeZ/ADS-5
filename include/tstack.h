deea865099e3c9d4559fcdfa5906233e2932c616
4yMHouDeZ <sergoart1234@gmail.com>
2026-04-06 16:57:01 +0300

    sergey

--- include/tstack.h
+++ include/tstack.h
@@ -4,20 +4,14 @@
 #define SIZE_MIN 100
 #define SIZE_MAX 500
 
-template<typename T, int size>
+template<typename T,  int size>
 class TStack {
  private:
   int top;
-  T data;
+  T data[size];
 
  public:
-  TStack(): top(-1) {
-    if (size <= 100) {
-      T data[SIZE_MIN];
-    } else {
-      T data[SIZE_MAX];
-    }
-  }
+  TStack(): top(-1) {}
 
   bool isEmpty() {
     return top == -1;
