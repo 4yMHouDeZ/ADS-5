69c6c3412863350e55fe94ff9f9465449362c8b9
4yMHouDeZ <sergoart1234@gmail.com>
2026-04-06 16:33:59 +0300

    sergey

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,37 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+    int top;
+    T data[size];
+  public:
+    TStack(): top(-1) {}
+
+    bool isEmpty() {
+      return top == -1;
+    }
+
+    bool isFull() {
+      return (top >= size);
+    }
+
+    void push(T el) {
+      if (!this->isFull()) {
+        top++;
+        data[top] = el;
+      }
+    }
+
+    void pop() {
+      if (!this->isEmpty()) {
+        data[top] = 0;
+        top--;
+      }
+    }
+
+    T peek() {
+      return data[top];
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,99 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+bool isOperation(char x) {
+  return (x > 41 && x < 48);
 }
 
-int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+bool isDigit(char x) {
+  return (x >= '0' && x <= '9');
+}
+
+void separate(std::string& str) {
+  if (str[str.length() - 1] != ' ' && str.length() > 0) str += ' ';
+}
+
+int ch2i(char x) {
+  return x - '0';
+}
+
+std::string infx2pstfx(const std::string& inf) {
+  TStack<char, 100> stack;
+  char el;
+  std::string result = "";
+  for (int i = 0; i < inf.length(); i++) {
+    el = inf[i];
+
+    if (el == '(') {
+      separate(result);
+      stack.push(el);
+    }
+
+    else if (isOperation(el)) {
+      separate(result);
+      if (((el == '-' || el == '+') && (stack.peek() == '-' || stack.peek() == '+')) || ((el == '/' || el == '*') && (stack.peek() == '/' || stack.peek() == '*')) || ((el == '-' || el == '+') && (stack.peek() == '*' || stack.peek() == '/'))) {
+        result += stack.peek();
+        stack.pop();
+      }
+      stack.push(el);
+      separate(result);
+    }
+
+    else if (isDigit(el)) result += el;
+
+    else if (el == ')') {
+      while (stack.peek() != '(') {
+        separate(result);
+        result += stack.peek();
+        stack.pop();
+      }
+      stack.pop();
+    }
+  }
+  while (!stack.isEmpty()) {
+    separate(result);
+    result += stack.peek();
+    stack.pop();
+  }
+  return result;
+}
+
+
+int eval(const std::string& post) {
+  TStack<int, 100> stack;
+  char el;
+  int a, b, temp;
+  for (int i = 0; i < post.length(); i++) {
+    el = post[i];
+    
+    if (isDigit(el)) {
+      if (i > 0 && isDigit(post[i-1])) {
+        temp = stack.peek() * 10 + ch2i(el);
+        stack.pop();
+        stack.push(temp);
+      } else {
+        stack.push(ch2i(el));
+      }
+    } else if (isOperation(el)) {
+      b = stack.peek();
+      stack.pop();
+      a = stack.peek();
+      stack.pop();
+      switch (el) {
+        case '+':
+          stack.push(a + b);
+          break;
+        case '-':
+          stack.push(a - b);
+          break;
+        case '*':
+          stack.push(a * b);
+          break;
+        case '/':
+          stack.push(a / b);
+          break;
+      }
+    }
+  }
+  return stack.peek();
 }
